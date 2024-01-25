a2c35a7eecd0ae30d8d94f9e31d5874f7eec2419
Matt Lui <Mattclui@gmail.com>
2024-01-25 14:48:31 -0800

    path

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-app.use(express.static('public'));
+const path = require("path")
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
